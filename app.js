23c1e1f3c06c60db53c819ce50c3c2bff1172c82
Nasir <ibnabdiwahid9@gmail.com>
2022-01-01 17:15:10 -0800

    Alpha (#1)
    * Get,Post,Put,Delete req
    
    * Replace incremental Id with UUID
    
    Co-authored-by: Nasir Abdiwahid <ibnabdiawhid9@gmail.com>

--- app.js
+++ app.js
@@ -1,47 +1,78 @@
-
-var express = require('express');
-var lodash = require('lodash');
-
-let allTask =[
-  {
-    "task": "bathroom",
-    "status": "done"
-  },
-  {
-    "task": "car",
-    "status": "not done"
-  },
-  {
-    "task": "laundry",
-    "status": ""
-  }
-]
-var app = express();
+const { countReset } = require('console');
+const express = require('express');
+const Joi = require('joi');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
 app.use(express.json());
-app.get('/', function (req, res) {
-    res.send('hello world')
+const {send} = require('process');
+app.set('view engine','ejs');
+
+const  allTask = [];
+
+app.get('/', (req, res) => {
+    res.render('index');
 }) 
-app.get('/todo' , function(req,res) {
+app.get('/allTask' ,(req,res) => {
   res.json(allTask);
     
 })
-app.post('/todo' , function(req,res) {
-  
-  allTask.push(req.body)
-  console.log(req.task);
+
+app.get('/allTask/:id', (req,res) => {
+  const task = allTask.find(c => c.id === req.params.id);
+  if(!task) res.status(404).send("Task wasnt found");
+  res.send(task);
+
+});
+
+
+app.post('/api/allTask', (req,res) => {
+  const schema = { 
+    task : Joi.string().min(3).required()
+} ;
+  const result = Joi.validate(req.body,schema);
+  if (result.error){
+    res.send(400).send(result.error.details[0].message) 
+    return;
+
+ };
+ const task = {
+   id: uuidv4(),
+   task : req.body.task,
+   status: "done"
+ };
+ allTask.push(task);
+ res.send(task);
   res.send('okay')
+});
+
+app.put('/allTask/:id', (req,res) => {
+  const task =  allTask.find(c => c.id === req.params.id)
+  if(!task) res.status(400).send("course cant be found");
+
+  const schema = {
+    task : Joi.string().min(3).required()
+};
+
+  const result = Joi.validate(req.body,schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message)
+    return;
+}
+  task.task = req.body.task;
+  res.send(task);
+
+
+});
+
+app.delete('/allTask/:id', (req,res) => {
+  const task =  allTask.find(c => c.id === req.params.id);
+  if(!task) res.status(400).send("course cant be found");
+
+  const index = allTask.indexOf(task)
+  allTask.splice(index,1);
+  res.send(task);
+
 })
-
-app.put('/todo', function(req,res){
-  const task = req.body.task
-  const status = req.body.status
-  const index = lodash.findIndex(allTask,{"task":task})
-  allTask.splice(index,1,req.body)
-  console.log(task)
-  res.send('Okay')
-})
-
-
 
 console.log("the server has started on localhost 8080");
 app.listen('8080');
